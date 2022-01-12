5609d94b2de2ed2d00732902a4d14904a7378f0a
MichalKroupa <kroupa.misak@email.cz>
2022-01-12 15:41:24 +0100

    test

--- Bulls_Cows.py
+++ Bulls_Cows.py
@@ -1,1 +1,18 @@
-print("test")
+import random
+
+def generuj_cislo():
+    random_number = random.randint(1000, 9999)
+
+def zkontroluj_cislo():
+    pass
+
+def main():
+    print("Hi there!")
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    print(oddelovac)
+    hadane_cislo = input("Enter a number: ")
+    print(oddelovac)
+
+oddelovac = "-" * 47
+main()
