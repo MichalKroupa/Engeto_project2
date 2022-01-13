34f5f873a89f0ce23d9979746f87d8107557dc8f
MichalKroupa <kroupa.misak@email.cz>
2022-01-13 15:07:26 +0100

    Rozpracovany Projekt TODO: dodelat cow/cows, pocitadlo

--- Bulls_Cows.py
+++ Bulls_Cows.py
@@ -1,18 +1,47 @@
 import random
 
 def generuj_cislo():
-    random_number = random.randint(1000, 9999)
+    # random_number = random.randint(1000, 9999)
+    random_number = [2, 0, 1, 7]
+    return random_number
 
-def zkontroluj_cislo():
-    pass
+def zkontroluj_cislo(hadane_cislo, random_number):
+    bulls_cows = [0, 0]
+    if hadane_cislo == random_number:
+        return True
+    else:
+        for index, cislo in enumerate(hadane_cislo):
+            if int(cislo) == int(random_number[index]):
+                bulls_cows[0] += 1
+            elif cislo in random_number:
+                bulls_cows[1] += 1
+        print(f"Bulls: {bulls_cows[0]}, Cows: {bulls_cows[1]}")
+        return False
+
+    
+def preved_cislo(hadane_cislo):
+    list_cislo = []
+    for i in str(hadane_cislo):
+        list_cislo.append(int(i))
+    return list_cislo
 
 def main():
+    guesses = 1
+    game_running = True
+    random_number = generuj_cislo()
     print("Hi there!")
     print("I've generated a random 4 digit number for you.")
     print("Let's play a bulls and cows game.")
-    print(oddelovac)
-    hadane_cislo = input("Enter a number: ")
-    print(oddelovac)
-
+    while game_running:
+        print(oddelovac)
+        hadane_cislo = int(input("Enter a number: "))
+        print(oddelovac)
+        hadane_cislo = preved_cislo(hadane_cislo)
+        if zkontroluj_cislo(hadane_cislo, random_number) is True:
+            print("Correct, you've guessed the right number")
+            print(f"In {guesses} guesses!")
+            game_running = False
+        else:
+            guesses += 1
 oddelovac = "-" * 47
 main()
